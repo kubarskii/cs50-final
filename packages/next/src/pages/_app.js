5a4ae5b058d503646ab1d9263931733b6a9e28f1
Александр Кубарский <sasha.kub95@gmail.com>
2022-11-30 17:18:51 +0300

    feature, ME-8 simple header fix

--- packages/next/src/pages/_app.js
+++ packages/next/src/pages/_app.js
@@ -25,7 +25,6 @@ export default function App({ Component, pageProps }) {
         <Head>
           <title>Message me!</title>
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-          <script src="/sw.js" defer />
         </Head>
         <Component {...pageProps} />
       </RouteGuard>
